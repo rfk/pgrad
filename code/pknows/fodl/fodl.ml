86f3685a967f797178d7369d6ee82b00abdc111a
rfk <rfk@fa295fee-cef6-0310-bd21-8564f560acc4>
2008-07-18 05:19:17 +0000

    git-svn-id: svn+ssh://rfk.id.au/storage/svnroot/uni/pgrad1/trunk@649 fa295fee-cef6-0310-bd21-8564f560acc4

---
+++ code/pknows/fodl/fodl.ml
@@ -0,0 +1,295 @@
+(*
+  Theorem Prover for our simplified FODL language, based on
+  the standard "pdlMark" prover from TWB.
+
+  We have added a new program type [ ! vassign ] which assigns values
+  to variables in the enclosed program.  It's based purely on string
+  re-writing and so is incredibly fragile, but works for our purposes.
+
+  We have also added a collection of tableaux rules to push vassigns inside
+  other program modalities, culimating with a string-replacing rule to apply
+  them to raw formulae.
+*)
+
+CONNECTIVES
+[ "~";"&";"v";"->";"<->";"<";">";"[";"]";"U";"*";";";"?";"!";"<="]
+
+GRAMMAR
+
+vassign :=
+      ATOM <= ATOM
+    | vassign ! vassign
+;;
+
+program := 
+      * program
+    | ? formula
+    | program U program
+    | program ; program
+    | ! vassign
+    | ATOM
+;;
+
+formula :=
+     ATOM | Verum | Falsum
+    | formula & formula
+    | formula v formula
+    | formula -> formula
+    | formula <-> formula
+    | < program > formula 
+    | [ program ] formula 
+    | ~ formula
+;;
+
+expr := formula;;
+END
+
+
+open FodlRewrite
+open FodlFunctions
+
+
+HISTORIES
+  HCore  : ListFormulaSet.olist := new ListFormulaSet.olist
+END
+
+VARIABLES
+  uev : FormulaIntSet.set := new FormulaIntSet.set;
+  mrk : bool := false
+END
+
+let nnf = List.map nnf_term
+let vsubs pattn = 
+  match pattn with 
+  | (vpattn,fpattn) -> [ vsubs1 (List.hd vpattn,List.hd fpattn) ]
+  ;;
+
+let vmerge pattn = 
+  match pattn with
+  | (vpattn1,vpattn2,fpattn) -> [ vmerge1 (List.hd vpattn1,List.hd vpattn2, List.hd fpattn) ]
+
+
+TABLEAU
+
+  (*  Termination Rules  *)
+
+  RULE Id { P } ; { ~ P } == Stop
+  BACKTRACK [
+      uev := uevundef ();
+      mrk := true
+  ]
+  END
+
+  RULE False { Falsum } == Stop
+  BACKTRACK [
+      uev := uevundef ();
+      mrk := true
+  ]
+  END
+
+  (*  standard pdlMark rules  *)
+
+  RULE And { A & B } ; Z === A ; B ; Z 
+  BACKTRACK [ uev := set_uev_All(uev@1, Z) ]
+  END
+
+  RULE UnionBox { [ A U B ] P } ; Z === [ A ] P ;  [ B ] P ; Z
+  BACKTRACK [ uev := set_uev_All(uev@1, Z) ]
+  END
+
+  RULE SeqBox { [ A ; B ] P } ; Z === [ A ] [ B ] P ; Z
+  BACKTRACK [ uev := set_uev_All(uev@1, Z) ]
+  END
+
+  RULE StarBox { [ * A ] P } ; Z === P ; [ A ] [ * A ] P ; Z 
+  BACKTRACK [ uev := set_uev_All(uev@1, Z) ]
+  END
+
+  RULE TestDia { < ? F > P } ; Z === F ; P ; Z
+  BACKTRACK [ uev := set_uev_Inh(uev@1, < ? F > P, P, Z) ]
+  END
+
+  RULE SeqDia { < A ; B > P } ; Z === < A > < B > P ; Z
+  BACKTRACK [ uev := set_uev_Inh(uev@1, < A ; B > P, < A > < B > P, Z) ]
+  END
+
+  RULE Or
+        { P v Q } ; Z 
+  ====================
+      P ; Z ||| Q ; Z
+
+  BRANCH [ [ doNextChild_disj(mrk@1, uev@1) ] ]
+  BACKTRACK [
+      uev := uev_disj_all(mrk@all, uev@all, Z);
+      mrk := mrk_disj(mrk@all)
+  ]
+  END
+
+  RULE TestBox
+  { [ ? F ] P } ; Z === nnf ( ~ F ) ; Z ||| P ; Z
+
+  BRANCH [ [ doNextChild_disj(mrk@1, uev@1) ] ]
+  BACKTRACK [
+      uev := uev_disj_all(mrk@all, uev@all, Z);
+      mrk := mrk_disj(mrk@all)
+  ]
+  END
+
+  RULE UnionDia
+  { < A U B > P } ; Z === < A > P ; Z ||| < B > P ; Z
+
+  BRANCH [ [ doNextChild_disj(mrk@1, uev@1) ] ]
+  BACKTRACK [
+      uev := uev_disj_union(mrk@all, uev@all, < A U B > P, < A > P, < B > P, Z);
+      mrk := mrk_disj(mrk@all)
+  ]
+  END
+
+  RULE StarDia
+  { < * A > P } ; Z === P ; Z ||| < A > < * A > P ; Z 
+
+  BRANCH [ [ doNextChild_disj(mrk@1, uev@1) ] ]
+  BACKTRACK [
+      uev := uev_disj_star(mrk@all, uev@all, < * A > P , P, < A > < * A > P, Z);
+      mrk := mrk_disj(mrk@all)
+  ]
+  END
+
+  (*  assignment-handling rules  *)
+
+  RULE VDia { < ! V > P } ; Z === [ ! V ] P ; Z
+  BACKTRACK [ uev := set_uev_All(uev@1, Z) ]
+  END
+
+  RULE VRawDia { [ ! V ] < A > P } ; Z === < A > [ ! V ] P ; Z
+  BACKTRACK [ uev := set_uev_All(uev@1, Z) ]
+  END
+
+  RULE VRawBox { [ ! V ] [ A ] P } ; Z === [ A ] [ ! V ] P ; Z
+  BACKTRACK [ uev := set_uev_All(uev@1, Z) ]
+  END
+
+  RULE VRawFml { [ ! V ] P } ; Z === vsubs ( V , P ) ; Z
+  BACKTRACK [ uev := set_uev_All(uev@1, Z) ]
+  END
+
+  RULE VMerge { [ ! V ] [ ! X ] P } ; Z === vmerge (V, X, P) ; Z
+  BACKTRACK [ uev := set_uev_All(uev@1, Z) ]
+  END
+
+  RULE VAnd { [ ! V ] ( A & B ) } ; Z === [ ! V ] A ; [ ! V ] B ; Z 
+  BACKTRACK [ uev := set_uev_All(uev@1, Z) ]
+  END
+
+  RULE VUnionBox { [ ! V ] [ A U B ] P } ; Z === [ ! V ] [ A ] P ;  [ ! V ] [ B ] P ; Z
+  BACKTRACK [ uev := set_uev_All(uev@1, Z) ]
+  END
+
+  RULE VSeqBox { [ ! V ] [ A ; B ] P } ; Z === [! V ] [ A ] [ B ] P ; Z
+  BACKTRACK [ uev := set_uev_All(uev@1, Z) ]
+  END
+
+  RULE VStarBox { [ ! V ] [ * A ] P } ; Z === [ ! V ] P ; [ ! V ] [ A ] [ * A ] P ; Z 
+  BACKTRACK [ uev := set_uev_All(uev@1, Z) ]
+  END
+
+  RULE VTestDia { [ ! V ] < ? F > P } ; Z === vsubs ( V , F ) ; [ ! V ] P ; Z
+  BACKTRACK [ uev := set_uev_Inh(uev@1, [ ! V ] < ? F > P, [ ! V ] P, Z) ]
+  END
+
+  RULE VSeqDia { [ ! V ] < A ; B > P } ; Z === [ ! V ] < A > < B > P ; Z
+  BACKTRACK [ uev := set_uev_Inh(uev@1, [ ! V ] < A ; B > P, [ ! V ] < A > < B > P, Z) ]
+  END
+
+  RULE VOr
+        { [ ! V ] ( P v Q ) } ; Z 
+  ======================================
+      [ ! V ] P ; Z ||| [ ! V ] Q ; Z
+
+  BRANCH [ [ doNextChild_disj(mrk@1, uev@1) ] ]
+  BACKTRACK [
+      uev := uev_disj_all(mrk@all, uev@all, Z);
+      mrk := mrk_disj(mrk@all)
+  ]
+  END
+
+  RULE VTestBox
+  { [! V ] [ ? F ] P } ; Z === nnf ( vsubs ( V , ~ F ) ) ; Z ||| [ ! V ] P ; Z
+
+  BRANCH [ [ doNextChild_disj(mrk@1, uev@1) ] ]
+  BACKTRACK [
+      uev := uev_disj_all(mrk@all, uev@all, Z);
+      mrk := mrk_disj(mrk@all)
+  ]
+  END
+
+  RULE VUnionDia
+  { [ ! V ] < A U B > P } ; Z === [ ! V ] < A > P ; Z ||| [ ! V ] < B > P ; Z
+
+  BRANCH [ [ doNextChild_disj(mrk@1, uev@1) ] ]
+  BACKTRACK [
+      uev := uev_disj_union(mrk@all, uev@all, [ ! V ] < A U B > P, [ ! V ] < A > P, [ ! V ] < B > P, Z);
+      mrk := mrk_disj(mrk@all)
+  ]
+  END
+
+  RULE VStarDia
+  { [ ! V ] < * A > P } ; Z === [ ! V ] P ; Z ||| [ ! V ] < A > < * A > P ; Z 
+
+  BRANCH [ [ doNextChild_disj(mrk@1, uev@1) ] ]
+  BACKTRACK [
+      uev := uev_disj_star(mrk@all, uev@all, [ ! V ] < * A > P , [ ! V ] P, [ ! V ] < A > < * A > P, Z);
+      mrk := mrk_disj(mrk@all)
+  ]
+  END
+
+  (*  looping and modality-removing rules  *)
+
+  RULE K 
+  { < A > P } ; [ A ] Y ; < B > E ; [ C ] F ; Z
+  ==============================================
+       P ; Y ||| [ A ] Y ; < B > E ; [ C ] F
+
+  COND   [ loop_check(P, Y, HCore) ]
+  ACTION [ [ HCore := push(P, Y, HCore) ] ; [] ]
+  BRANCH [ [ test_ext(mrk@1, uev@1, P, HCore) ] ]
+  BACKTRACK [
+      uev := uev_ext(mrk@all, uev@all, < A > P, P);
+      mrk := mrk_ext(mrk@all)
+  ]
+  CACHE := true
+  END
+
+  RULE Loop
+       < A > X ; [ B ] Y
+       ==================
+             Stop
+
+  BACKTRACK [
+      uev := uev_loop(< A > X, [ B ] Y, HCore);
+      mrk := false
+  ]
+  CACHE := true
+  END
+
+END
+
+STRATEGY := 
+  let term = tactic (False ! Id) in 
+  let vnorm = tactic (VDia ! VRawDia ! VRawBox ! VMerge) in
+  let decom = tactic (And ! StarBox ! UnionBox ! SeqDia ! SeqBox ! TestDia) in
+  let vdecom = tactic (VAnd ! VStarBox ! VUnionBox ! VSeqDia ! VSeqBox ! VTestDia) in
+  let split = tactic (Or ! StarDia ! UnionDia ! TestBox) in
+  let vsplit = tactic (VOr ! VStarDia ! VUnionDia ! VTestBox) in
+  let sat = tactic (term ! vnorm ! decom ! vdecom ! split ! vsplit ! VRawFml)
+  in tactic ((sat ! K ! Loop)*)
+
+let exit = function
+  | true -> "Closed"
+  | false -> "Open"
+
+PP := List.map nnf_snf_term
+NEG := List.map neg_term
+EXIT := exit (mrk@1)
+
+MAIN
---
+++ code/pknows/fodl/fodlRewrite.ml
@@ -0,0 +1,137 @@
+source Fodl
+
+let neg_term = function formula ( a ) -> formula ( ~ a )
+
+let rec nnf_term = function
+  | formula ( a & b ) -> formula ( {nnf_term a} & {nnf_term b} )
+  | formula ( ~ ( a & b ) ) ->
+      formula ( {nnf_term formula ( ~ a )} v {nnf_term formula ( ~ b )} )
+  | formula ( a v b ) -> formula ({nnf_term a} v {nnf_term b})
+  | formula ( ~ ( a v b ) ) ->
+      formula ( {nnf_term formula ( ~ a )} & {nnf_term formula ( ~ b )} )
+  | formula ( a <-> b ) ->
+      formula ( {nnf_term formula ( a -> b )} & {nnf_term formula ( b -> a )} )
+  | formula ( ~ ( a <-> b ) ) ->
+      formula ( {nnf_term formula ( ~ (a -> b) )} v {nnf_term formula ( ~ (b -> a) )} )
+  | formula ( a -> b ) -> nnf_term formula ( (~ a) v b )
+  | formula ( ~ (a -> b) ) -> nnf_term formula ( a & (~ b) )
+  | formula ( ~ ~ a ) -> nnf_term a
+  | formula ( < p > a ) -> formula ( < {nnf_prog p} > {nnf_term a} )
+  | formula ( ~ ( < p > a ) ) -> formula ( [ {nnf_prog p} ] {nnf_term ( formula ( ~ a ) )} )
+  | formula ( [ p ] a ) -> formula ( [ {nnf_prog p} ] {nnf_term a} )
+  | formula ( ~ ( [ p ] a ) ) -> formula ( < {nnf_prog p} > {nnf_term ( formula ( ~ a ) )} )
+  | formula (Verum) as f -> f
+  | formula (Falsum) as f -> f
+  | formula (~ Verum) -> formula (Falsum)
+  | formula (~ Falsum) -> formula (Verum)
+  | formula ( ~ A ) as f -> f
+  | formula ( A ) as f -> f
+and nnf_prog = function
+  | program ( * a ) -> program ( * {nnf_prog a} )
+  | program ( ? f ) -> program ( ? {nnf_term f} )
+  | program ( a U b ) -> program ( {nnf_prog a} U {nnf_prog b} )
+  | program ( a ; b ) -> program ( {nnf_prog a} ; {nnf_prog b} )
+  | _ as p -> p
+
+let rec eps_free = function
+  | program ( * a ) -> false
+  | program ( ? f ) -> false
+  | program ( a U b ) -> (eps_free a) && (eps_free b)
+  | program ( a ; b ) -> (eps_free a) || (eps_free b)
+  | _ -> true
+
+let rec snf_term = function
+  | formula ( a & b ) -> formula ( {snf_term a} & {snf_term b} )
+  | formula ( a v b ) -> formula ({snf_term a} v {snf_term b})
+  | formula ( < p > a ) -> formula ( < {snf_prog p} > {snf_term a} )
+  | formula ( [ p ] a ) -> formula ( [ {snf_prog p} ] {snf_term a} )
+  | formula ( a ) as f -> f
+and snf_prog = function
+  | program ( * a ) ->
+      begin 
+        match ef_prog a with
+        | None -> program ( ? Verum )
+        | Some a1 -> program ( * a1 )
+      end
+  | program ( ? f ) -> program ( ? {snf_term f} )
+  | program ( a U b ) -> program ( {snf_prog a} U {snf_prog b} )
+  | program ( a ; b ) -> program ( {snf_prog a} ; {snf_prog b} )
+  | _ as p -> p
+and ef_prog = function
+  | program ( * a ) ->
+      begin
+        match ef_prog a with
+        | None -> None
+        | Some a1 -> Some (program ( a1 ; ( * a1 ) ))
+      end
+  | program ( ? f ) -> None
+  | program ( a U b ) ->
+      begin
+        match (ef_prog a, ef_prog b) with
+        | (None, ob1) -> ob1
+        | (oa1, None) -> oa1
+        | (Some a1, Some b1) -> Some (program ( a1 U b1 ))
+      end
+  | program ( a ; b ) ->
+      let sa = snf_prog a in
+      let sb = snf_prog b in
+      if eps_free a || eps_free b then Some (program ( sa ; sb ))
+      else
+        begin
+          match (ef_prog a, ef_prog b) with
+          | (None, None) -> None
+          | (None, Some b1) -> Some (program ( sa ; b1 ))
+          | (Some a1, None) -> Some (program ( a1 ; sb ))
+          | (Some a1, Some b1) -> Some (program ( ( a1 ; sb ) U ( sa ; b1 ) ))
+        end
+  | _ as p -> Some p
+
+let nnf_snf_term f =
+  let nnf = nnf_term f in
+  let snf = snf_term nnf in
+  (* print_endline (formula_printer snf); *)
+  snf
+
+
+(*  we assume that we only subs into raw formulae, not formulae with
+    modalities - the other cases are just here to keep the typechecker happy,
+    and they leave the formula unmodified 
+*)
+
+let vsubs_str oldval newval s  =
+    Str.global_replace (Str.regexp_string oldval) newval s
+
+let vsubs_atom (`Atom text,`Atom name,`Atom value) =
+    vsubs_str name value text
+
+let rec vsubs_do = function
+  | ( formula ( a & b ) , o,n ) -> formula ( { vsubs_do (a,o,n) } & { vsubs_do (b,o,n) } )
+  | ( formula ( a v b ) , o,n ) -> formula ( { vsubs_do (a,o,n) } v { vsubs_do (b,o,n) } )
+  | ( formula ( ~ a ) , o,n ) -> formula ( ~ { vsubs_do (a,o,n) } )
+  | ( formula ( a -> b ) , o,n ) ->  formula ( { vsubs_do (a,o,n) } -> { vsubs_do (b,o,n) } )
+  | ( formula ( a <-> b ) , o,n ) ->  formula ( { vsubs_do (a,o,n) } <-> { vsubs_do (b,o,n) } )
+  | ( formula ( < p > a ) as f , o,n ) -> f
+  | ( formula ( [ p ] a ) as f , o,n ) -> f
+  | ( formula (Verum) as f, o,n ) -> f
+  | ( formula (Falsum) as f, o,n ) -> f
+  | ( formula (A) as f , o,n ) -> formula ( {`Atom (vsubs_atom(f,o,n)) } )
+
+let rec vsubs1 = function
+  | ( vassign ( A <= B ) , f ) -> formula ( { vsubs_do(f,a,b) } )
+  | ( vassign ( a ! b ) , f ) -> formula ( { vsubs1( b , vsubs1(a,f) ) } )
+
+let rec vcontains = function
+  | ( vassign ( A <= B ) , vv ) -> if vv=a then true else false
+  | ( vassign ( a ! b ), vv ) -> if vcontains(a,vv) then true
+                                 else vcontains(b,vv)
+
+let rec vmerge_do = function
+  | ( vassign ( A <= B ) as v1 , v2 ) -> 
+                                   let merged = vassign ( v2 ! v1)
+                                   in if vcontains(v2,a) then v2 else merged
+  | ( vassign (a ! b ) , v2 ) -> vmerge_do(b,vmerge_do(a,v2))
+
+let rec vmerge1 = function
+  | ( v1, v2, f) -> let merged = vassign ( { vmerge_do(v1,v2) } )
+                    in formula ( [ ! merged ] f )
+
